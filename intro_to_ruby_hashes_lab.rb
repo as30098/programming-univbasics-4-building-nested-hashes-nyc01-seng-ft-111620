733fac20454f0014bb180bf57c0e6d85a04598f1
Alexandra Spencer <alexspencer421@gmail.com>
2020-11-07 19:38:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,9 +11,8 @@ def monopoly_with_second_tier
   # complexity.
   {:railroads => 
   {:pieces => 4, 
-  :rent_in_dollars => {},
-  :names => {},
-  :one_piece_owned => 25
+  :rent_in_dollars => {:one_piece_owned => 25},
+  :names => {}
   } 
   } 
 end
